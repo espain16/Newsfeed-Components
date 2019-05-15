f7a2658f27a62fa9f38a5bea4bbea6cdd8f0e212
Elise Spain <elisespain@gmail.com>
2019-05-15 15:17:14 -0700

    class menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -5,6 +5,12 @@ const toggleMenu = () => {
 
 // Start Here: Create a reference to the ".menu" class
 const menu;
+class Menu {
+  constructor(menu){
+    this.menu = menu;
+    this.
+  }
+}
 // create a reference to the ".menu-button" class
 const menuButton;
 // Using your menuButton reference, add a click handler that calls toggleMenu
